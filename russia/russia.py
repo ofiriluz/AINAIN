9fe7a0ec3a0ef85e4a9bf53c04cdd879e75eb4d6
Ofir Iluz <iluzofir@gmail.com>
2018-04-27 11:47:45 +0300

    Converted to 2.7 py and added setup.py for exe

--- russia/russia.py
+++ russia/russia.py
@@ -1,4 +1,5 @@
-import winreg
+# PYTHON 2.7 ONLY
+import _winreg as winreg
 import ctypes
 import pygame
 import threading
@@ -17,7 +18,7 @@ def startAudioAsync():
 
 def getAgilBzinRegValue():
     hKey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, 
-                          "Software\\Russia\\RusBzin")
+                          "SOFTWARE\\Russia\\RusBzin",  0, (winreg.KEY_WOW64_64KEY + winreg.KEY_ALL_ACCESS))
     result = winreg.QueryValueEx(hKey, "AgilBzin")
     return result[0]
 
@@ -26,12 +27,12 @@ def showMessage(msg, title="Error", flags=0):
 
 def performAgilBaValue(val):
     if val == SUCC_VAL:
-        showMessage("/control/solveThisToMaster1337", "Cool and Good", 32)
+        showMessage(u"/control/solveThisToMaster1337", u"Cool and Good", 32)
     elif val == SUCC_REAL_VAL:
         threading.Thread(target=startAudioAsync).start()
-        showMessage("Coordinates:\nLat = 32.0629879\nLon = 34.7789279", "420", 64)
+        showMessage(u"Coordinates:\nLat = 32.0629879\nLon = 34.7789279", u"420", 64)
     else:
-        showMessage("Invalid Key, program failure.", "Error", 16)
+        showMessage(u"Invalid Key, program failure.", u"Error", 16)
 
 value = getAgilBzinRegValue()
 performAgilBaValue(value)
